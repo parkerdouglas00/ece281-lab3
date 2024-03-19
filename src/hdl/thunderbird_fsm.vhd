55d24e9acd1bc3136defdb367ae4744e1638c602
parkerdouglas00 <144307564+parkerdouglas00@users.noreply.github.com>
2024-03-19 13:39:35 -0600

    Completed through waveform

--- src/hdl/thunderbird_fsm.vhd
+++ src/hdl/thunderbird_fsm.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thunderbird_fsm.vhd
---| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson
---| CREATED       : 03/2017 Last modified 06/25/2020
+--| AUTHOR(S)     : Capt Phillip Warner, Capt Dan Johnson, C3C Parker Douglas
+--| CREATED       : 03/2017 Last modified 19 Mar 2024
 --| DESCRIPTION   : This file implements the ECE 281 Lab 2 Thunderbird tail lights
 --|					FSM using enumerated types.  This was used to create the
 --|					erroneous sim for GR1
@@ -36,18 +36,18 @@
 --|					can be changed by the inputs
 --|					
 --|
---|                 xxx State Encoding key
+--|                 Binary State Encoding key
 --|                 --------------------
 --|                  State | Encoding
 --|                 --------------------
---|                  OFF   | 
---|                  ON    | 
---|                  R1    | 
---|                  R2    | 
---|                  R3    | 
---|                  L1    | 
---|                  L2    | 
---|                  L3    | 
+--|                  OFF   | 000
+--|                  ON    | 001
+--|                  R1    | 010
+--|                  R2    | 011
+--|                  R3    | 100
+--|                  L1    | 101
+--|                  L2    | 110
+--|                  L3    | 111
 --|                 --------------------
 --|
 --|
@@ -87,21 +87,75 @@ library ieee;
  
 entity thunderbird_fsm is 
   port(
-	
+    i_clk, i_reset  : in    std_logic;
+    i_left, i_right : in    std_logic;
+    o_lights_L      : out   std_logic_vector(2 downto 0);
+    o_lights_R      : out   std_logic_vector(2 downto 0)
   );
 end thunderbird_fsm;
 
 architecture thunderbird_fsm_arch of thunderbird_fsm is 
 
 -- CONSTANTS ------------------------------------------------------------------
+
+    signal f_S          : std_logic_vector (2 downto 0);
+    signal f_S_next     : std_logic_vector (2 downto 0);
   
 begin
 
 	-- CONCURRENT STATEMENTS --------------------------------------------------------	
+	-- Next State Logic
+	f_S_next(2) <= ( not f_S(2) and not f_S(1) and not f_S(0) and i_left and not i_right ) or
+	               ( not f_S(2) and f_S(1) and f_S(0) ) or
+	               ( f_S(2) and not f_S(1) and f_S(0) ) or
+	               ( f_S(2) and f_S(1) and not f_S(0) );
+	f_S_next(1) <= ( not f_S(2) and not f_S(1) and not f_S(0) and not i_left and i_right ) or
+	               ( not f_S(2) and f_S(1) and not f_S(0) ) or
+	               ( f_S(2) and not f_S(1) and f_S(0) ) or
+	               ( f_S(2) and f_S(1) and not f_S(0) );
+    f_S_next(0) <= ( not f_S(2) and not f_S(1) and not f_S(0) and i_left and not i_right ) or
+                   ( not f_S(2) and not f_S(1) and not f_S(0) and i_left and i_right ) or
+                   ( not f_S(2) and f_S(1) and not f_S(0) ) or
+                   ( f_S(2) and f_S(1) and not f_S(0) );
+    
+    -- Output Logic
+    o_lights_L(2) <= ( not f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( f_S(2) and f_S(1) and f_S(0) );
+                     
+    o_lights_L(1) <= ( not f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( f_S(2) and f_S(1) and not f_S(0) ) or
+                     ( f_S(2) and f_S(1) and f_S(0) );
+                     
+    o_lights_L(0) <= ( not f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( f_S(2) and f_S(1) and not f_S(0) ) or
+                     ( f_S(2) and f_S(1) and f_S(0) );
+    
+    o_lights_R(0) <= ( not f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( not f_S(2) and f_S(1) and not f_S(0) ) or
+                     ( not f_S(2) and f_S(1) and f_S(0) ) or
+                     ( f_S(2) and not f_S(1) and not f_S(0) );
+
+    o_lights_R(1) <= ( not f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( not f_S(2) and f_S(1) and f_S(0) ) or
+                     ( f_S(2) and not f_S(1) and not f_S(0) );
+                     
+    o_lights_R(2) <= ( not f_S(2) and not f_S(1) and f_S(0) ) or
+                     ( f_S(2) and not f_S(1) and not f_S(0) );
+                     
 	
     ---------------------------------------------------------------------------------
 	
 	-- PROCESSES --------------------------------------------------------------------
+	
+	   register_proc : process (i_clk, i_reset)
+    begin
+        if i_reset = '1' then
+            f_S <= "000"; --reset state is OFF
+        elsif (rising_edge(i_clk)) then
+            f_S <= f_S_next; --next state becomes current state
+        end if;
+    end process register_proc;
     
 	-----------------------------------------------------					   
 				  
--- src/hdl/thunderbird_fsm_tb.vhd
+++ src/hdl/thunderbird_fsm_tb.vhd
@@ -11,8 +11,8 @@
 --| ---------------------------------------------------------------------------
 --|
 --| FILENAME      : thunderbird_fsm_tb.vhd (TEST BENCH)
---| AUTHOR(S)     : Capt Phillip Warner
---| CREATED       : 03/2017
+--| AUTHOR(S)     : Capt Phillip Warner, C3C Parker Douglas
+--| CREATED       : 03/2017 - Modified 19 Mar 2024
 --| DESCRIPTION   : This file tests the thunderbird_fsm modules.
 --|
 --|
@@ -58,27 +58,163 @@ architecture test_bench of thunderbird_fsm_tb is
 	
 	component thunderbird_fsm is 
 	  port(
-		
-	  );
+        i_clk, i_reset  : in    std_logic;
+        i_left, i_right : in    std_logic;
+        o_lights_L      : out   std_logic_vector(2 downto 0);
+        o_lights_R      : out   std_logic_vector(2 downto 0)
+      );
 	end component thunderbird_fsm;
 
 	-- test I/O signals
+	-- input
+	signal w_clk       : std_logic := '0';
+	signal w_reset     : std_logic := '0';
+	signal w_left      : std_logic := '0';
+	signal w_right     : std_logic := '0';
+	-- output
+	signal w_lights_L  : std_logic_vector (2 downto 0) := "000";
+	signal w_lights_R  : std_logic_vector (2 downto 0) := "000";
 	
 	-- constants
+	constant k_clk_period : time := 10 ns;
 	
 	
 begin
 	-- PORT MAPS ----------------------------------------
 	
+	uut : thunderbird_fsm
+	    port map (
+	       i_clk => w_clk,
+	       i_reset => w_reset,
+	       i_left => w_left,
+	       i_right => w_right,
+	       o_lights_L => w_lights_L,
+	       o_lights_R => w_lights_R
+        );
+	
 	-----------------------------------------------------
 	
 	-- PROCESSES ----------------------------------------	
     -- Clock process ------------------------------------
+    clk_proc : process
+    begin
+            w_clk <= '0';
+        wait for k_clk_period/2;
+            w_clk <= '1';
+        wait for k_clk_period/2;
+    end process;
     
 	-----------------------------------------------------
 	
 	-- Test Plan Process --------------------------------
+	sim_proc : process
+	begin
 	
+	--sequential timing
+	w_reset <= '1';
+	wait for k_clk_period*1;
+	   assert w_lights_L = "000" report "bad reset L" severity failure;
+	   assert w_lights_R = "000" report "bad reset R" severity failure;
+	
+	w_reset <= '0';
+	wait for k_clk_period*1;
+	
+	-- left on
+	w_left <= '1';
+	wait for k_clk_period*1;
+	   assert w_lights_L = "001" report "bad left on L 1" severity failure;
+	   assert w_lights_R = "000" report "bad left on R 1" severity failure;
+	
+	wait for k_clk_period*1;
+       assert w_lights_L = "011" report "bad left on L 2" severity failure;
+       assert w_lights_R = "000" report "bad left on R 2" severity failure;
+       
+	wait for k_clk_period*1;
+       assert w_lights_L = "111" report "bad left on L 3" severity failure;
+       assert w_lights_R = "000" report "bad left on R 3" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad left on L 4" severity failure;
+       assert w_lights_R = "000" report "bad left on R 4" severity failure;
+    
+	wait for k_clk_period*1;
+       assert w_lights_L = "001" report "bad left on L 5" severity failure;
+       assert w_lights_R = "000" report "bad left on R 5" severity failure;
+ 
+     --left off
+       w_left <= '0';
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "011" report "bad left off L 1" severity failure;
+       assert w_lights_R = "000" report "bad left off R 1" severity failure;
+       
+    wait for k_clk_period*1;
+       assert w_lights_L = "111" report "bad left off L 2" severity failure;
+       assert w_lights_R = "000" report "bad left off R 2" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad left off L 3" severity failure;
+       assert w_lights_R = "000" report "bad left off R 3" severity failure;
+
+-------------------------------------------------------------------------------------
+    --right on
+	w_right <= '1';
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right on L 1" severity failure;
+       assert w_lights_R = "001" report "bad right on R 1" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right on L 2" severity failure;
+       assert w_lights_R = "011" report "bad right on R 2" severity failure;
+       
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right on L 3" severity failure;
+       assert w_lights_R = "111" report "bad right on R 3" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right on L 4" severity failure;
+       assert w_lights_R = "000" report "bad right on R 4" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right on L 5" severity failure;
+       assert w_lights_R = "001" report "bad right on R 5" severity failure;
+
+    --right off
+    w_right <= '0';
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right off L 1" severity failure;
+       assert w_lights_R = "011" report "bad right off R 1" severity failure;
+       
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right off L 2" severity failure;
+       assert w_lights_R = "111" report "bad right off R 2" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad right off L 3" severity failure;
+       assert w_lights_R = "000" report "bad right off R 3" severity failure;
+
+----------------------------------------------------------------------------
+    -- ON State
+    w_left <= '1';
+    w_right <= '1';
+    wait for k_clk_period*1;
+       assert w_lights_L = "111" report "bad on state L 1" severity failure;
+       assert w_lights_R = "111" report "bad on state R 1" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad on state L 2" severity failure;
+       assert w_lights_R = "000" report "bad on state R 2" severity failure;
+    
+    wait for k_clk_period*1;
+       assert w_lights_L = "111" report "bad on state L 3" severity failure;
+       assert w_lights_R = "111" report "bad on state R 3" severity failure;
+
+    wait for k_clk_period*1;
+       assert w_lights_L = "000" report "bad on state L 4" severity failure;
+       assert w_lights_R = "000" report "bad on state R 4" severity failure;
 	-----------------------------------------------------	
+	   wait;
+	end process;
 	
 end test_bench;
